0186ace57fcb8e482c79bc8f653a8d67566f20a2
Luis <luis@staudt-fridingen.de>
2024-04-15 15:05:05 +0200

    added InterfaceInjectionScanner

--- agent/src/main/java/net/luis/agent/Agent.java
+++ agent/src/main/java/net/luis/agent/Agent.java
@@ -1,5 +1,6 @@
 package net.luis.agent;
 
+import net.luis.asm.InterfaceInjectionScanner;
 
 import java.lang.instrument.Instrumentation;
 import java.lang.instrument.UnmodifiableClassException;
@@ -14,6 +15,7 @@ public class Agent {
 	
 	public static void premain(String agentArgs, Instrumentation inst) throws UnmodifiableClassException {
 		System.out.println("Agent loaded!");
+		inst.addTransformer(new InterfaceInjectionScanner(inst), true);
 		
 		
 		
