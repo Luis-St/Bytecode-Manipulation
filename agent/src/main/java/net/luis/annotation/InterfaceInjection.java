3766c586cb5464ecfb9179ba665f10e7cccbed8f
Luis <luis@staudt-fridingen.de>
2024-04-15 15:04:15 +0200

    added target and retention to interface injection

--- agent/src/main/java/net/luis/annotation/InterfaceInjection.java
+++ agent/src/main/java/net/luis/annotation/InterfaceInjection.java
@@ -1,5 +1,9 @@
 package net.luis.annotation;
 
+import java.lang.annotation.*;
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.CLASS)
 public @interface InterfaceInjection {
 	
 	Class<?>[] targets();
