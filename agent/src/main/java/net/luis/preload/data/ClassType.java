0af7035bb4ca46aa463da943f9b4e30f5e625a82
Luis <luis@staudt-fridingen.de>
2024-04-18 21:35:18 +0200

    added ClassType enum

---
+++ agent/src/main/java/net/luis/preload/data/ClassType.java
@@ -0,0 +1,37 @@
+package net.luis.preload.data;
+
+import org.objectweb.asm.Opcodes;
+
+/**
+ *
+ * @author Luis-St
+ *
+ */
+
+public enum ClassType {
+	
+	CLASS(-1),
+	ANNOTATION(Opcodes.ACC_ANNOTATION),
+	INTERFACE(Opcodes.ACC_INTERFACE),
+	ENUM(Opcodes.ACC_ENUM),
+	RECORD(Opcodes.ACC_RECORD),
+	MODULE(Opcodes.ACC_MODULE);
+	
+	private final int value;
+	
+	ClassType(int value) {
+		this.value = value;
+	}
+	
+	public static ClassType fromAccess(int access) {
+		for (ClassType type : values()) {
+			if (type == CLASS) {
+				continue;
+			}
+			if ((access & type.value) != 0) {
+				return type;
+			}
+		}
+		return CLASS;
+	}
+}
