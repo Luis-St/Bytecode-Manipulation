e5fbe4b9cf5093014ee67df7f0e22df98ab58284
Luis <luis@staudt-fridingen.de>
2024-05-05 19:08:18 +0200

    added PatternTransformer

---
+++ agent/src/main/java/net/luis/agent/asm/transformer/PatternTransformer.java
@@ -0,0 +1,155 @@
+package net.luis.agent.asm.transformer;
+
+import net.luis.agent.asm.ASMUtils;
+import net.luis.agent.asm.base.BaseClassTransformer;
+import net.luis.agent.asm.base.visitor.BaseClassVisitor;
+import net.luis.agent.asm.base.visitor.BaseMethodVisitor;
+import net.luis.agent.preload.PreloadContext;
+import net.luis.agent.preload.data.*;
+import net.luis.agent.preload.type.TypeModifier;
+import net.luis.agent.util.Utils;
+import org.intellij.lang.annotations.Pattern;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.objectweb.asm.*;
+import org.objectweb.asm.commons.LocalVariablesSorter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ *
+ * @author Luis-St
+ *
+ */
+
+public class PatternTransformer extends BaseClassTransformer {
+	
+	private static final Type PATTERN = Type.getType(Pattern.class);
+	private static final Type STRING = Type.getType(String.class);
+	
+	private final PreloadContext context;
+	
+	public PatternTransformer(@NotNull PreloadContext context) {
+		this.context = context;
+	}
+	
+	private boolean checkReturn(@NotNull MethodData method) {
+		return method.isMethod() && method.getReturnType().equals(STRING) && method.isAnnotatedWith(PATTERN);
+	}
+	
+	@Override
+	@SuppressWarnings("DuplicatedCode")
+	protected boolean shouldIgnore(@NotNull Type type) {
+		ClassContent content = this.context.getClassContent(type);
+		return (content.methods().stream().filter(method -> !method.is(TypeModifier.ABSTRACT)).map(MethodData::parameters).flatMap(List::stream).noneMatch(parameter -> parameter.isAnnotatedWith(PATTERN)) &&
+			content.methods().stream().noneMatch(this::checkReturn)) || super.shouldIgnore(type);
+	}
+	
+	@Override
+	protected int getClassWriterFlags() {
+		return ClassWriter.COMPUTE_FRAMES;
+	}
+	
+	@Override
+	protected @NotNull ClassVisitor visit(@NotNull Type type, @Nullable Class<?> clazz, @NotNull ClassReader reader, @NotNull ClassWriter writer) {
+		ClassContent content = this.context.getClassContent(type);
+		Runnable markedModified = () -> this.modified = true;
+		return new BaseClassVisitor(writer) {
+			@Override
+			public @NotNull MethodVisitor visitMethod(int access, @NotNull String name, @NotNull String descriptor, @Nullable String signature, String @Nullable [] exceptions) {
+				MethodVisitor visitor = super.visitMethod(access, name, descriptor, signature, exceptions);
+				MethodData method = content.getMethod(name, Type.getType(descriptor));
+				boolean checkReturn = PatternTransformer.this.checkReturn(method);
+				if (method.is(TypeModifier.ABSTRACT) || !(method.parameters().stream().anyMatch(parameter -> parameter.isAnnotatedWith(PATTERN)) || checkReturn)) {
+					return visitor;
+				}
+				LocalVariablesSorter sorter = new LocalVariablesSorter(access, descriptor, visitor);
+				return new PatternVisitor(type, sorter, method, !checkReturn, markedModified);
+			}
+		};
+	}
+	
+	private static class PatternVisitor extends BaseMethodVisitor {
+		
+		private final List<ParameterData> lookup = new ArrayList<>();
+		private final Type type;
+		private final LocalVariablesSorter sorter;
+		private final MethodData method;
+		private final Runnable markedModified;
+		private final boolean ignoreReturn;
+		
+		@SuppressWarnings("DuplicatedCode")
+		private PatternVisitor(@NotNull Type type, @NotNull LocalVariablesSorter visitor, @NotNull MethodData method, boolean ignoreReturn, Runnable markedModified) {
+			super(visitor);
+			this.type = type;
+			this.sorter = visitor;
+			this.method = method;
+			this.ignoreReturn = ignoreReturn;
+			this.markedModified = markedModified;
+			method.parameters().stream().filter(parameter -> parameter.isAnnotatedWith(PATTERN)).forEach(this.lookup::add);
+		}
+		
+		private @NotNull String getMessage(@NotNull ParameterData parameter, @NotNull String pattern) {
+			if (parameter.isNamed()) {
+				return Utils.capitalize(parameter.name()) + " must match pattern '" + pattern + "'";
+			}
+			return ASMUtils.getSimpleName(parameter.type()) + " (parameter #" + parameter.index() + ") must match pattern '" + pattern + "'";
+		}
+		
+		private void visitPatternCheck(@NotNull String regex, int index, @NotNull Label label) {
+			this.mv.visitLdcInsn(regex);
+			this.mv.visitMethodInsn(Opcodes.INVOKESTATIC, "java/util/regex/Pattern", "compile", "(Ljava/lang/String;)Ljava/util/regex/Pattern;", false);
+			this.mv.visitVarInsn(Opcodes.ALOAD, index);
+			this.mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/util/regex/Pattern", "matcher", "(Ljava/lang/CharSequence;)Ljava/util/regex/Matcher;", false);
+			this.mv.visitMethodInsn(Opcodes.INVOKEVIRTUAL, "java/util/regex/Matcher", "matches", "()Z", false);
+			this.mv.visitJumpInsn(Opcodes.IFNE, label);
+		}
+		
+		private void visitException(@NotNull String message) {
+			this.mv.visitTypeInsn(Opcodes.NEW, "java/lang/IllegalArgumentException");
+			this.mv.visitInsn(Opcodes.DUP);
+			this.mv.visitLdcInsn(message);
+			this.mv.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/IllegalArgumentException", "<init>", "(Ljava/lang/String;)V", false);
+			this.mv.visitInsn(Opcodes.ATHROW);
+		}
+		
+		@Override
+		public void visitCode() {
+			this.mv.visitCode();
+			boolean isStatic = this.method.is(TypeModifier.STATIC);
+			for (ParameterData parameter : this.lookup) {
+				Label label = new Label();
+				String value = parameter.getAnnotation(PATTERN).get("value");
+				
+				this.visitPatternCheck(value, isStatic ? parameter.index() : parameter.index() + 1, label);
+				this.visitException(this.getMessage(parameter, value));
+				
+				this.mv.visitJumpInsn(Opcodes.GOTO, label);
+				this.mv.visitLabel(label);
+				this.markedModified.run();
+			}
+		}
+		
+		@Override
+		public void visitInsn(int opcode) {
+			if (opcode != Opcodes.ARETURN || this.ignoreReturn) {
+				this.mv.visitInsn(opcode);
+				return;
+			}
+			Label label = new Label();
+			String value = this.method.getAnnotation(PATTERN).get("value");
+			int local = this.sorter.newLocal(this.method.getReturnType());
+			this.mv.visitVarInsn(Opcodes.ASTORE, local);
+			
+			this.visitPatternCheck(value, local, label);
+			this.visitException("Method " + ASMUtils.getSimpleName(this.type) + "#" + this.method.name() + " return value must match pattern '" + value + "'");
+			
+			this.mv.visitJumpInsn(Opcodes.GOTO, label);
+			this.mv.visitLabel(label);
+			this.mv.visitVarInsn(Opcodes.ALOAD, local);
+			this.mv.visitInsn(opcode);
+			this.markedModified.run();
+		}
+	}
+}
--- src/main/java/net/luis/Main.java
+++ src/main/java/net/luis/Main.java
@@ -3,8 +3,11 @@ package net.luis;
 import net.luis.agent.annotation.Default;
 import net.luis.utils.logging.LoggerConfiguration;
 import net.luis.utils.logging.LoggingType;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.Level;
+import org.intellij.lang.annotations.Pattern;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.*;
 
@@ -30,7 +33,7 @@ public final class Main {
 	public static void main(@Default @NotNull String[] args) {
 		LoggerConfiguration logger = new LoggerConfiguration("*");
 		
-		execute(null, null, null);
+		execute("ls", null, null);
 		if (logger instanceof MyInterface my) {
 			System.out.println("LoggerConfiguration is an instance of MyInterface!");
 			System.out.println(my.build().getName());
@@ -47,9 +50,19 @@ public final class Main {
 		}
 	}
 	
-	public static void execute(@Default("ls") String command, @Default("[]") String[] args, @Default List<String> values) {
+	public static void execute(@Pattern("^[a-z]*$") String command, @Default("[]") @NotNull String[] args, @Default List<String> values) {
 		System.out.println("Command: " + command);
 		System.out.println("Args: " + Arrays.toString(args));
 		System.out.println("Values: " + values);
+	}
+	
+	public static @NotNull @Pattern("^.*$") String getExtension(@Nullable String file) {
+		String str = StringUtils.stripToEmpty(file);
+		int index = str.lastIndexOf(".");
+		if (index == -1) {
+			return "";
+		} else {
+			return str.substring(index + 1);
+		}
 	}
 }
