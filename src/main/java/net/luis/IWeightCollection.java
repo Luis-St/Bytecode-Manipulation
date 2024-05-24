6e264c121027b44e9778c7fe6c34e7a640ec5051
Luis <luis@staudt-fridingen.de>
2024-05-24 14:59:52 +0200

    added injector test cases

--- src/main/java/net/luis/IWeightCollection.java
+++ src/main/java/net/luis/IWeightCollection.java
@@ -7,9 +7,19 @@ import net.luis.utils.collection.WeightCollection;
 @InjectInterface(targets = WeightCollection.class)
 public interface IWeightCollection {
 	
+	@Injector(method = "<init>(Random)", target = "Maps#newTreeMap()")
+	default void injectConstructor() {
+		System.out.println("Injecting constructor cancellation in WeightCollection#<init>");
+	}
+	
 	@Injector(target = "NavigableMap#put(Object, Object)")
 	default boolean injectAdd() {
 		System.out.println("Injecting add cancellation in WeightCollection#add");
 		return true;
+	}
+	
+	@Injector(target = "IllegalStateException#<init>(String)")
+	default void injectNext() {
+		System.out.println("Injecting next cancellation in WeightCollection#next");
 	}
 }
